e7a9e1a8ffab352e5c8103e952b4cd2d3de5743c
Haduin <36509113+Haduin@users.noreply.github.com>
2021-11-09 00:06:10 +0100

    EL-126, Added 10 mocks to migration APP_USER (#48)

--- src/main/resources/db/migration/h2/V1_2_0__insert_data.sql
+++ src/main/resources/db/migration/h2/V1_2_0__insert_data.sql
@@ -1,8 +1,14 @@
+--OFFER
 INSERT INTO OFFER (BACKGROUND_DESCRIPTION,FIRST_NAME,LAST_NAME,LONG_DESCRIPTION,PRICE,RATE_DESCRIPTION,SHORT_DESCRIPTION,UUID) VALUES
 ('Doświadczony programista','Karol','Milabiwski','Super korepetycję z Javy','150','Za 1h','Korepetycje Java', '2b733549-d2cc-40f0-b7f3-9bfa9f3c1b89');
-
-
-
-
-
-
+--APP_USER
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('IXHOf10EodSqriYg7vuD0gYeda42','9diego.venutooo@ttmgss.com');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('CHMyE7FN8MdpKRt69Ny2Z9kpaE32','zkhaled0@niwod.com');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('wGTIM9otBAetTwzxh14fU9g4NMm1','3josias.dias@tunnelerph.com');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('Xw85IA89WOXHXUkXrmWSC6VzSb72','karol@op.pl');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('5lfgnYp1AWYozNj2PfEG1g3ivoz2','adye47@hotmail.com');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('pAhZcDO7zxdzFvkDJoFKgWUFSxP2','ehack55464f@oymuloe.com');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('lds2TKJhGgdYlBGiNlIpOqEcTdn1','tfoad.ramziz@aloxy.ml');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('WZAmVKdEWrQa7UhMRQydgyRJEbZ2','xsarfarazahme@docd.site');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('OtJl0KrvTMQ7c3LrgWtzXXTXcAw1','1abu@nbobd.com');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('QWp0rdRlDjOHKSMjomYBG7bCxKg1','lsoma@halumail.com');
