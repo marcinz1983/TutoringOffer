82d346c618e86c93b1dd4e215a8f3a8e4470b924
marcinz1983 <65621940+marcinz1983@users.noreply.github.com>
2021-11-15 06:30:51 +0000

    add mock offers to flyway migration (#56)
    Co-authored-by: Marcin <marcinzielinski1983@gmail.com>

--- src/main/resources/db/migration/h2/V1_2_0__insert_data.sql
+++ src/main/resources/db/migration/h2/V1_2_0__insert_data.sql
@@ -1,14 +1,26 @@
---OFFER
-INSERT INTO OFFER (BACKGROUND_DESCRIPTION,FIRST_NAME,LAST_NAME,LONG_DESCRIPTION,PRICE,RATE_DESCRIPTION,SHORT_DESCRIPTION,UUID) VALUES
-('Doświadczony programista','Karol','Milabiwski','Super korepetycję z Javy','150','Za 1h','Korepetycje Java', '2b733549-d2cc-40f0-b7f3-9bfa9f3c1b89');
 --APP_USER
-Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('IXHOf10EodSqriYg7vuD0gYeda42','9diego.venutooo@ttmgss.com');
-Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('CHMyE7FN8MdpKRt69Ny2Z9kpaE32','zkhaled0@niwod.com');
-Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('wGTIM9otBAetTwzxh14fU9g4NMm1','3josias.dias@tunnelerph.com');
-Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('Xw85IA89WOXHXUkXrmWSC6VzSb72','karol@op.pl');
-Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('5lfgnYp1AWYozNj2PfEG1g3ivoz2','adye47@hotmail.com');
-Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('pAhZcDO7zxdzFvkDJoFKgWUFSxP2','ehack55464f@oymuloe.com');
-Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('lds2TKJhGgdYlBGiNlIpOqEcTdn1','tfoad.ramziz@aloxy.ml');
-Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('WZAmVKdEWrQa7UhMRQydgyRJEbZ2','xsarfarazahme@docd.site');
-Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('OtJl0KrvTMQ7c3LrgWtzXXTXcAw1','1abu@nbobd.com');
-Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL) VALUES ('QWp0rdRlDjOHKSMjomYBG7bCxKg1','lsoma@halumail.com');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL,FIREBASE_EMAIL_VERIFIED) VALUES ('IXHOf10EodSqriYg7vuD0gYeda42','9diego.venutooo@ttmgss.com','false');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL,FIREBASE_EMAIL_VERIFIED) VALUES ('CHMyE7FN8MdpKRt69Ny2Z9kpaE32','zkhaled0@niwod.com','false');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL,FIREBASE_EMAIL_VERIFIED) VALUES ('wGTIM9otBAetTwzxh14fU9g4NMm1','3josias.dias@tunnelerph.com','false');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL,FIREBASE_EMAIL_VERIFIED) VALUES ('Xw85IA89WOXHXUkXrmWSC6VzSb72','karol@op.pl','false');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL,FIREBASE_EMAIL_VERIFIED) VALUES ('5lfgnYp1AWYozNj2PfEG1g3ivoz2','adye47@hotmail.com','false');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL,FIREBASE_EMAIL_VERIFIED) VALUES ('pAhZcDO7zxdzFvkDJoFKgWUFSxP2','ehack55464f@oymuloe.com','false');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL,FIREBASE_EMAIL_VERIFIED) VALUES ('lds2TKJhGgdYlBGiNlIpOqEcTdn1','tfoad.ramziz@aloxy.ml','false');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL,FIREBASE_EMAIL_VERIFIED) VALUES ('WZAmVKdEWrQa7UhMRQydgyRJEbZ2','xsarfarazahme@docd.site','false');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL,FIREBASE_EMAIL_VERIFIED) VALUES ('OtJl0KrvTMQ7c3LrgWtzXXTXcAw1','1abu@nbobd.com','false');
+Insert into APP_USER (FIREBASE_UID,FIREBASE_EMAIL,FIREBASE_EMAIL_VERIFIED) VALUES ('QWp0rdRlDjOHKSMjomYBG7bCxKg1','lsoma@halumail.com','false');
+
+
+--OFFER
+INSERT INTO OFFER (BACKGROUND_DESCRIPTION,FIRST_NAME,LAST_NAME,LONG_DESCRIPTION,PRICE,RATE_DESCRIPTION,SHORT_DESCRIPTION,UUID,APP_USER_ID) VALUES
+('Doświadczony programista','Karol','Milabiwski','Super korepetycję z Javy','150','Za 1h','Korepetycje Java', '2b733549-d2cc-40f0-b7f3-9bfa9f3c1b89','1'),
+('Senior','Artur','XXXXXXXX','Super korepetycję z Python','50','Za 1h','Korepetycje Python', 'be50b00f-ec34-499b-90f2-fac351cb7630','2'),
+('java developer','Marek','YYYYYYYY','Super korepetycję z C#','100','Za 1h','Korepetycje C#', 'ba90cb75-0657-4c56-890d-7c6f65ebd677','3'),
+('Doświadczony programista','Filip','ZZZZZZZZZZ','Super korepetycję z Javy','150','Za 1h','Korepetycje Java', '46682d5a-57be-4494-bad3-3604991593a1','4'),
+('Doświadczony programista','Tomasz','TTTTTT','Super korepetycję z Javy','150','Za 1h','Korepetycje Java', '3719721b-22da-4d39-9baa-82e91c71275b','5'),
+('Doświadczony programista','Krzysztof','EEEEEEEEE','Super korepetycję z Javy','150','Za 1h','Korepetycje Java', '14925687-428a-4585-8e5b-430ad4d90880','6'),
+('Doświadczony programista','Michal','UUUUUUU','Super korepetycję z Javy','130','Za 1h','Korepetycje Java', '665f16ab-c2e9-45a5-b2d4-69b53d68962e','7'),
+('Doświadczony programista','Jakub','RRRRRRR','Super korepetycję z Javy','150','Za 1h','Korepetycje Java', '3a0d2c5e-7840-4697-87a7-4eaec0ee17fb','8'),
+('Doświadczony programista','Lukasz','IIIIIIII','Super korepetycję z C#','120','Za 1h','Korepetycje Java', 'e537d801-e1e9-4503-890b-e4f006f0db5d','9'),
+('Doświadczony programista','Pawel','WWWWWW','Super korepetycję z Javy','150','Za 1h','Korepetycje Java', '8425730d-f14d-4b24-aa5e-da7ccc5a1615','10'),
+('Doświadczony programista','Wieslaw','UUUUUUU','Super korepetycję z Javy','150','Za 1h','Korepetycje Java', 'e2a01496-30cb-4dba-8fe6-f9d7d17c5269','2');
